e311e32820b19361710699935f218ab23e9c7461
shash1578 <shashankraut1829@gmail.com>
2026-02-01 15:09:13 +0530

    student and warden dashboard

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ wardenBtn.addEventListener("click", () => {
     studentBtn.classList.remove("active");
     roleInput.value = "warden";
 });
+
+function login(event) {
+    event.preventDefault();
+
+    const role = document.getElementById("role").value;
+
+    if (role === "student") {
+        window.location.href = "student.html";
+    } else if (role === "warden") {
+        window.location.href = "warden.html";
+    } else {
+        alert("Please select a role");
+    }
+}
+function showSection(sectionId) {
+    const sections = document.querySelectorAll(".section");
+    sections.forEach(sec => sec.style.display = "none");
+
+    document.getElementById(sectionId).style.display = "block";
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
